1d5304960b5fd51327e42fb732172462c0749935
tigromov <102591826+tigromov@users.noreply.github.com>
2022-11-18 16:57:30 +0600

    делаю ссылки, найти подходящий атрибут чтобы их спарсить

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -66,8 +68,9 @@ public class Main {
         ////////////////////////////////////////////////////////АВТОРИЗАЦИЯ В ЛИЧНОМ КАБИНЕТЕ////////////////////////////////
         webDriver.get("https://kaspi.kz/mc/");
 
-        Thread.sleep(1000);
+        Thread.sleep(5000);
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div/div/form/div[1]/div/div/section/div/nav/ul/li[2]/a/span")).click();
+        Thread.sleep(2000);
         webDriver.findElement(By.id("user_email")).sendKeys("zakaz@invask.kz");
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div/div/form/div[2]/div/button")).click();
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div/div/form/div[1]/div/div/div[1]/input")).sendKeys("!ZakaZ2022" + Keys.ENTER);
@@ -83,36 +86,93 @@ public class Main {
 
         /////////////////////////////////////////////////////ПАРСИНГ СТРАНИЦЫ ТОВАРОВ//////////////////////////////////////////
 
-        ////////////////////////////////////////////////////ПОЛУЧЕНИЕ АРТИКУЛА/////////////////////////////////////////////////
-
+        int prodCounter = 0;
+        int prodCounterTotal = 0;
         Document doc = Jsoup.parse(webDriver.getPageSource());
-        Elements page = doc.getElementsByClass("subtitle is-6");
-        ArrayList skus = new ArrayList();
-        for (Element element :page) {
-            String firstProduct = element.text();
-            List productStringList = Arrays.asList(firstProduct.split(" "));
-            int numberOFWorlds = productStringList.size();
-            String articule = (String) productStringList.get(numberOFWorlds - 1);
-            skus.add(articule);
+       // System.out.println(doc);
+        String numberOfPages = doc.getElementsByClass("pagination-list").text();
+
+        int productsListSize = Integer.parseInt(numberOfPages.substring(6, 8));
+        System.out.println("Всего страниц с товарами в каспи магазине: " + productsListSize);
+
+
+
+       /////////////////////////////////////////////ЦИКЛ ПО СТРАНИЦАМ ТОВАРОВ/////////////////////////////////////////
+
+       // for (int i = 0; i < productsListSize; i++) {
+
+            ////////////////////////////////////////////////////ПОЛУЧЕНИЕ АРТИКУЛА/////////////////////////////////////////////////
+            Elements productsDescriptions = doc.getElementsByClass("subtitle is-6");
+            ArrayList skusList = new ArrayList();
+            for (Element element : productsDescriptions) {
+                String productDescription = element.text();
+               // System.out.println(productDescription); тест
+                List worldsInLineOfProductDescriptionList = Arrays.asList(productDescription.split(" "));
+                int numberOFWorlds = worldsInLineOfProductDescriptionList.size();
+                String article = (String) worldsInLineOfProductDescriptionList.get(numberOFWorlds - 1);
+                skusList.add(article);
+            }
+            int skusListSize = skusList.size();
+
+            System.out.println("Получены артикулы: " + skusList);
+
+            ///////////////////////////////////////////////Запись 10ти артикулов в файл//////////////////////////////////////
+             for (int j = 0; j < skusListSize; j++) {
+                midSheet.createRow(prodCounterTotal + 1 + j).createCell(0).setCellValue((String) skusList.get(j));
+                    }
+
+
+             ///////////////////////////////////////////////переход по ссылкам///////////////////////////////////////////////
+
+            Elements Links = doc.getElementsByClass("is-5");
+        System.out.println(Links.attr("abs:href"));
+        ArrayList test = new ArrayList();
+        for (Element link : Links) {
+            System.out.println(link.attr("abs:href"));
         }
-        System.out.println(skus);
+
+       //     test.add(link.select("a").attr("href"));}
 
 
 
+//            if (String.valueOf(link.select("a").attr("href")) !="  "){
+//           // webDriver.get(link.select("a").attr("href"));
+//            System.out.println(link.select("a").attr("href"));}
+//            Thread.sleep(1000);
+
+ //       }
+        System.out.println(Links.size());
 
 
-
-//        ///////////////////////////////////////////////////////////ЗАПИCЬ В ФАЙЛ //////////////////////////////////////////////
-//        midWb.write(fos);
-//        fos.close();
-//        System.out.println("Файл выгрузился по адресу: " + ParsOutput);
-//        //webDriver.close();
+//             System.out.println(Links);
 //
-//
-//        ///////////////////////////////////////////////////////////ВРЕМЯ ВЫПОЛНЕНИЯ ПРОГРАММЫ//////////////////////////////////
-//        ZonedDateTime finish = ZonedDateTime.now();
-//        double elapsed = (double)Duration.between(start,finish).toMinutes();
-//        System.out.println("Время выполнения программы: " + elapsed);
+
+
+
+
+
+
+
+
+
+
+
+        //System.out.println(prodCounter);
+     //   }   ///открыть скобку общего главного цикла
+
+
+
+        ///////////////////////////////////////////////////////////ЗАПИCЬ В ФАЙЛ //////////////////////////////////////////////
+        midWb.write(fos);
+        fos.close();
+        System.out.println("Файл выгрузился по адресу: " + ParsOutput);
+        //webDriver.close();
+
+
+        ///////////////////////////////////////////////////////////ВРЕМЯ ВЫПОЛНЕНИЯ ПРОГРАММЫ//////////////////////////////////
+        ZonedDateTime finish = ZonedDateTime.now();
+        double elapsed = (double) Duration.between(start,finish).toMinutes();
+        System.out.println("Время выполнения программы: " + elapsed + " минут");
 
 
 
